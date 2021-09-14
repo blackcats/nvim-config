00fe47322540a7d94d2f60da13cfaa245d4b0992
lgo <olegall@mediametrie.fr>
2021-09-14 12:12:49 +0200

    adding new lsp server

--- lua/blackcats/lsp.lua
+++ lua/blackcats/lsp.lua
@@ -1,18 +1,28 @@
 -- lsp config
 --
+-- To finlalize the configuration and the installation use the following links:
+-- * https://github.com/neovim/nvim-lspconfig
+-- * https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+-- and the links inside.
 
 local function on_attach()
     -- TODO.... :/
 end
 
 require'lspconfig'.bashls.setup{}
+
 require'lspconfig'.ansiblels.setup{}
 require'lspconfig'.terraformls.setup{}
--- to add
--- python
--- ruby
--- docker
--- json
--- perl
--- puppet
--- vimls...?
+require'lspconfig'.jsonls.setup{}
+
+require'lspconfig'.dockerls.setup{}
+-- To install and configure: https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#puppet
+-- require'lspconfig'.puppet.setup{}
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.vimls.setup{}
+-- To install and configure: https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#perlls
+-- require'lspconfig'.perlls.setup{}
+
+-- ruby -> view solargraph
+-- lua -> sumneko_lua
