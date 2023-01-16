edac44ec3ace725b94b7130272c5ee327fd96900
lgo <2762658+blackcats@users.noreply.github.com>
2023-01-16 04:49:04 +0100

    nvim: add configuration for new packages

---
+++ after/plugin/colors.lua
@@ -0,0 +1,18 @@
+-- Set themes color
+--
+require('rose-pine').setup({
+	--- @usage 'main' | 'moon'
+	-- dark_variant = 'moon',
+	disable_background = false
+})
+
+function MyColor(color) 
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+MyColor()
