5c5fdc8dab1eb356dd9d4c45693b40302d9e0a0f
lgo <olegall@mediametrie.fr>
2023-01-19 10:31:12 +0100

    nvim: add configuration for language packages

---
+++ after/plugin/terraform.lua
@@ -0,0 +1,7 @@
+-- terraform plugin configuration
+-- https://github.com/hashivim/vim-terraform/blob/master/doc/terraform.txt
+--
+local g = vim.g
+
+g.terraform_align = 1
+g.terraform_fmt_on_save = 1
