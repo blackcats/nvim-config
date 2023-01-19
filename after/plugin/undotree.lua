bad82e9cd9ee2bf7c1a9bbfcf2070ee0533fb8db
lgo <2762658+blackcats@users.noreply.github.com>
2023-01-19 13:18:01 +0100

    nvim(undotree): add auto focus and split width options

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -4,5 +4,7 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 vim.g.undotree_WindowLayout = 2
 vim.g.undotree_ShortIndicators = 0
+vim.g.undotree_SplitWidth = 30
 vim.g.undotree_DiffpaneHeight = 15
 vim.g.undotree_DiffCommand = "diff -u"
+vim.g.undotree_SetFocusWhenToggle = 1
