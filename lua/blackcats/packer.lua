720846a58bcc489d5414c1d9f0c3505f63170a94
lgo <olegall@mediametrie.fr>
2023-01-23 09:26:04 +0100

    nvim(packer): add themes and auto pairs / ending test plugin

--- lua/blackcats/packer.lua
+++ lua/blackcats/packer.lua
@@ -22,18 +22,35 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-    end
-  })
+  -- colors themes
+  use { 'rose-pine/neovim', as = 'rose-pine' }
+  use { 'navarasu/onedark.nvim' }
+  use { 'EdenEast/nightfox.nvim' }
+  use { 'projekt0n/github-nvim-theme', tag = 'v0.0.7' }
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use { 'morhetz/gruvbox' }
 
   use { 'nvim-lualine/lualine.nvim' }
 
   -- Install Treesitter
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+
+  -- End wise
+  use {
+    "RRethy/nvim-treesitter-endwise",
+    requires = "nvim-treesitter",
+    event = "InsertEnter",
+  }
+
+  -- Auto pairs
+  use {
+    "windwp/nvim-autopairs",
+    module = {
+      "nvim-autopairs.completion.cmp",
+      "nvim-autopairs"
+    },
+  }
+
 
   -- Install LSP
   use {
