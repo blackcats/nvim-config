d0d12357aca02b53e2130c019dc6607d55eea6be
lgo <olegall@mediametrie.fr>
2023-01-23 11:33:09 +0100

    nvim(theme): configure the differents color themes

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -6,8 +6,42 @@ require('rose-pine').setup({
 	disable_background = false
 })
 
-function MyColor(color) 
-	color = color or "rose-pine"
+-- onedark theme
+-- see github: navarasu/onedark.nvim
+require('onedark').setup({
+    style = 'cool',
+    transparent = true,
+})
+
+-- github-theme 
+-- see github: projekt0n/github-nvim-theme
+require('github-theme').setup({
+    theme_style = "dimmed",
+    transparent = true,
+})
+
+-- nightfox theme
+-- see: github EdenEast/nightfox.nvim
+require("nightfox").setup({
+    options = {
+        transparent = true,
+    }
+})
+
+-- catppuccin theme
+-- see github: catppuccin/nvim
+require("catppuccin").setup({
+    flavour = "macchiato",
+    -- flavour = "mocha",
+    transparent_background = true,
+})
+
+-- gruvbox theme configurations
+-- see github: morhetz/gruvbox
+vim.g.gruvbox_contrast_dark = "hard"
+
+function MyColor(color)
+	color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
