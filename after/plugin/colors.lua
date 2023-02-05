df20acc81758684ac8ccb87d05d8540705714e11
lgo <lgo@black-cats.org>
2023-02-05 18:22:09 +0100

    nvim: add kickstart configuration

---
+++ after/plugin/colors.lua
@@ -0,0 +1,56 @@
+-- Set themes color
+--
+
+-- rose-pine theme
+-- see github: rose-pine/neovim
+require('rose-pine').setup({
+	--- @usage 'main' | 'moon'
+	-- dark_variant = 'moon',
+	disable_background = false
+})
+
+-- onedark theme
+-- see github: navarasu/onedark.nvim
+require('onedark').setup({
+    style = 'cool',
+    transparent = true,
+})
+
+-- github-theme 
+-- see github: projekt0n/github-nvim-theme
+require('github-theme').setup({
+    theme_style = "dimmed",
+    transparent = true,
+})
+
+-- nightfox theme
+-- see: github EdenEast/nightfox.nvim
+require("nightfox").setup({
+    options = {
+        transparent = true,
+    }
+})
+
+-- catppuccin theme
+-- see github: catppuccin/nvim
+require("catppuccin").setup({
+    -- flavour = "macchiato",
+    flavour = "mocha",
+    transparent_background = false,
+})
+
+-- gruvbox theme configurations
+-- see github: morhetz/gruvbox
+vim.g.gruvbox_contrast_dark = "hard"
+
+function MyColor(color)
+	color = color or "catppuccin"
+	-- color = color or "onedark"
+	vim.cmd.colorscheme(color)
+
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+MyColor()
