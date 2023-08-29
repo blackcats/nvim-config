8bc7633266b8f0a108317381971efbdd425efcf3
lgo <lgo@black-cats.org>
2023-08-29 22:05:36 +0200

    feat(package): add vim-tmux-navigation package

--- lua/blackcats/packer.lua
+++ lua/blackcats/packer.lua
@@ -82,6 +82,29 @@ return require('packer').startup(function(use)
 
   -- tmux.conf vi mplufin
   use { 'tmux-plugins/vim-tmux' }
+  -- use { 'alexghergh/vim-tmux-navigator' }
+  -- use {
+  --       'alexghergh/vim-tmux-navigator',
+  --       config = function () require("nvim-tmux-navigation").setup() end
+  -- }
+  use { 'alexghergh/nvim-tmux-navigation', config = function()
+
+        local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+        nvim_tmux_nav.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+
+        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+    end
+ }
+
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
 
   -- Git plugins
